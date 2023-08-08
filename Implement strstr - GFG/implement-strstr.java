e17f9ee8e91a516c570d7bd7641287a5f505eba9
meetakashn <101237788+meetakashn@users.noreply.github.com>
2023-08-08 10:06:08 +0530

    Added solution - LeetHub

---
+++ Implement strstr - GFG/implement-strstr.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class Implement_strstr
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String line = sc.nextLine();
+			String a = line.split(" ")[0];
+			String b = line.split(" ")[1];
+			
+			GfG g = new GfG();
+			System.out.println(g.strstr(a,b));
+			
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class GfG
+{
+    //Function to locate the occurrence of the string x in the string s.
+    int strstr(String s, String x)
+    {
+        int i=0;
+		while(i<s.length()-x.length()+1){
+		   String r=s.substring(i,x.length()+i);
+		   if(r.equals(x)){return i;}
+		   i++;
+		}
+		return -1;
+    }
+}
